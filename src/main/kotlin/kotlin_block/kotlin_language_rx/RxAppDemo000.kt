f23fca8f0a5665192804ee6960f2ec7ec7f56317
NikolayIT <tkachenni@mail.ru>
2023-05-11 14:36:55 +0300

    Update kotlin languages  and algorithms

--- src/main/kotlin/kotlin_block/kotlin_language_rx/RxAppDemo000.kt
+++ src/main/kotlin/kotlin_block/kotlin_language_rx/RxAppDemo000.kt
@@ -1,11 +1,49 @@
 package kotlin_block.kotlin_language_rx
 
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.atomic.AtomicInteger
+
 fun main() {
+    testRxKotlin001()
 
-
+    //testRxKotlin002()
 }
 
 
 fun testRxKotlin001(){
+    val count = AtomicInteger()
 
+    Observable.range(1, 10)
+        //.doOnNext { println(Thread.currentThread().name) }
+        .map {
+            println(Thread.currentThread().name)
+            it
+        }
+        .subscribeOn(Schedulers.io())
+        .map {
+            println(Thread.currentThread().name)
+            it
+        }
+        .subscribeOn(Schedulers.computation())
+        .map {
+            println(Thread.currentThread().name)
+            it
+        }
+        .map { it -> it + 2 }
+        .observeOn(Schedulers.single())
+        .subscribe {
+            println(it)
+            println(Thread.currentThread().name)
+        }
+}
+
+fun testRxKotlin002() {
+    val count = AtomicInteger()
+    Observable.range(1, 10)
+        .doOnNext { ignored -> count.incrementAndGet() }
+        .ignoreElements()
+        .andThen(Single.just(count.get()))
+        .subscribe(System.out::println)
 }
